0344989b05d67474dcecb2e8d69012325626ac96
atomichorns <36160406+atomichorns@users.noreply.github.com>
2018-02-05 08:24:47 -0600

    Create game.js

---
+++ game.js
@@ -0,0 +1,20 @@
+/* Guess a number Game
+written 2/5/2018 by Rich Lewis */
+
+var guessNum;
+var guessRight = "You guessed it right!";
+var guessLow = "You guessed too low!  Try again.";
+var guessHigh = "You guessed too high!  Try again.";
+var num = Math.floor(Math.random() * 100);
+
+//Main loop;
+while guessNum != num {
+guessNum = prompt("Please guess a number between 1 and 100");
+  if guessNum > num {
+    alert(guessHigh);
+  }
+  else if guessNum < num {
+    alert(guessLow);
+  }
+}
+alert(guessRight);
